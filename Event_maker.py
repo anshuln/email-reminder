d5aacbee8665dc1435a5a01ad2eee6894c84ba25
anshuln <38972131+anshuln@users.noreply.github.com>
2018-05-04 12:04:55 +0530

    Update Event_maker.py

--- Event_maker.py
+++ Event_maker.py
@@ -1,3 +1,8 @@
+'''
+This project sifts through all you (IIT Bombay GPO) emails, and from student notices, extracts the date, time and venue of the various club 
+events which happen in the institute. It stores those in a .csv file and reminds you of any upcoming events you may be interested in.
+'''
+
 import Excel_integration
 
 Excel_integration.main()
